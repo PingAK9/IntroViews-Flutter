09d534383752745c330879bace30aaf218e87921
PingAK9 <khanhuitse05@gmail.com>
2020-03-19 15:22:22 +0700

    my custom

--- lib/UI/page_bubble.dart
+++ lib/UI/page_bubble.dart
@@ -17,8 +17,8 @@ class PageBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     if (viewModel.iconAssetPath == null && viewModel.bubbleInner == null) {
       return Container(
-        width: 55.0,
-        height: 65.0,
+        width: 6 + lerpDouble(5, 20, viewModel.activePercent),
+        height: 20.0,
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(10.0),
@@ -27,19 +27,12 @@ class PageBubble extends StatelessWidget {
               //This method return in between values according to active percent.
               height: 5,
               decoration: BoxDecoration(
-                shape: BoxShape.circle,
+                borderRadius: BorderRadius.circular(10),
                 //Alpha is used to create fade effect for background color
                 color: viewModel.isHollow
                     ? viewModel.bubbleBackgroundColor
-                        .withAlpha((0x88 * viewModel.activePercent).round())
-                    : viewModel.bubbleBackgroundColor,
-                border: Border.all(
-                  color: viewModel.isHollow
-                      ? viewModel.bubbleBackgroundColor.withAlpha(
-                          (0x88 * (1 - viewModel.activePercent)).round())
-                      : Colors.transparent,
-                  width: 1.0,
-                ), //Border
+                    : viewModel.bubbleBackgroundColor
+                        .withAlpha((0x88 * viewModel.activePercent).round()),
               ), //BoxDecoration
             ), //Container
           ), //Padding
